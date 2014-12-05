c9ae6ebb8fc33c33dac3869fa8fbdc2cef2c5a8b
Aurélien Rivet <aurelienrivet@hotmail.fr>
2014-12-06 00:12:12 +0100

    Adding the cstring library to base64.hpp

--- src/base64.hpp
+++ src/base64.hpp
@@ -2,6 +2,7 @@
 #define BASE64_HPP
 
 #include <cstdint>
+#include <cstring>
 
 void encode_block(const unsigned char *block, std::string& output);
 std::string base64_encode(const unsigned char *input, size_t size);
