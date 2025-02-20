d6e289dd66d177d6d702ae79dd7fe181d1448a8b
Aurélien Rivet <aurelienrivet@hotmail.fr>
2025-02-20 17:29:24 +0100

    fix accented characters

--- src/AudioFile.cpp
+++ src/AudioFile.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include <taglib/tstring.h>
+
 using namespace std;
 
 const TagLib::String PICTURE_KEY = "PICTURE";
@@ -31,15 +33,15 @@ void AudioFile::clearCover()
 void AudioFile::set(const string &tag, const string &value)
 {
     if(tag == "genre")
-        m_file->tag()->setGenre(value);
+        m_file->tag()->setGenre(TagLib::String(value, TagLib::String::Type::UTF8));
     else if(tag == "year")
         m_file->tag()->setYear(stoul(value));
     else if(tag == "album")
-        m_file->tag()->setAlbum(value);
+        m_file->tag()->setAlbum(TagLib::String(value, TagLib::String::Type::UTF8));
     else if(tag == "artist")
-        m_file->tag()->setArtist(value);
+        m_file->tag()->setArtist(TagLib::String(value, TagLib::String::Type::UTF8));
     else if(tag == "title")
-        m_file->tag()->setTitle(value);
+        m_file->tag()->setTitle(TagLib::String(value, TagLib::String::Type::UTF8));
     else if(tag == "trackNumber")
         m_file->tag()->setTrack(stoul(value));
     else if(tag == "cover")
